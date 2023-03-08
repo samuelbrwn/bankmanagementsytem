d9fcaad001c7357e2ebe638aa8a34b29fa2e6d0c
Samuel Brown <samuelbrwn97@gmail.com>
2023-03-08 11:41:29 -0700

    added new user and view user functions.

--- main.c
+++ main.c
@@ -7,14 +7,86 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+/*Global Declaration of the user struct*/
+struct user
+{
+    int acc_number;
+    char fname[20], lname[20];
+    char email[30];
+    int balance;
+};
+
 
 /*PROTOTYPES*/
 void welcome_screen();
+int new_user(void);
 void divider();
 void print_spaces(int x);
 void print_walls(int x);
 /*PROTOTYPES*/
 
+/*
+ * This function creates a new user and then writes
+ * this user to a dat file in binary.
+ */
+int new_user(void) {
+    struct user new_user;
+    new_user.balance = 0;
+    new_user.acc_number = rand()%10000+1;
+
+    FILE* fptr = fopen("user-records.dat", "a");
+    if (fptr == NULL) return -1;
+
+    system("cls");
+    printf("Your user number is: %d\n", new_user.acc_number);
+    divider();
+    printf("\nPlease enter your First Name: ");
+    scanf("%s", new_user.fname);
+    printf("\n");
+    divider();
+    printf("\nPlease enter your Last Name: ");
+    scanf("%s", new_user.lname);
+    printf("\n");
+    divider();
+    printf("\nPlease enter your email: ");
+    scanf("%s", new_user.email);
+    printf("\n");
+    divider();
+
+    fwrite(&new_user, sizeof (struct user), 1, fptr);
+    
+    fclose(fptr);
+    return 0;
+}
+
+/*
+ * This function opens the user-records.dat file, and fetches the
+ * structs and prints the data.
+ * 
+ * I have not yet implemented the ability to search for a specific user.
+ *
+ * */
+void view_account() {
+
+    system("cls");
+
+    struct user u;
+
+    FILE* fptr = fopen("user-records.dat", "r");
+    if (fptr == NULL) {
+        fprintf(stderr, "\nError opening user-records.dat\n\n");
+        exit(-1);
+    }
+
+    while(fread(&u, sizeof(struct user), 1, fptr))
+        printf("Account Number: %d\nName: %10s %10s\nUser Email Address: %20s\nYour Balance: %d\n\n", u.acc_number, u.fname, u.lname, u.email, u.balance);
+
+    fclose(fptr);
+}
+
+/*Function to create dividing walls of "---" when printing things.*/
 void divider() {
     int i = 0;
     for(i = 0;i<80;i++) {
@@ -38,6 +110,8 @@ void print_walls(int x) {
 }
 
 void welcome_screen() {
+    int choice=0;
+
     divider();
     printf("|\t ____    ____  ____   __  _       ___   _____       _____  ____  ___ ___\t|\n");
     printf("|\t|    \\  /    ||    \\ |  |/ ]     /   \\ |     |     / ___/ /    ||   |   |\t|\n");
@@ -54,6 +128,22 @@ void welcome_screen() {
     printf("|\t\t2. OPEN AN ACCOUNT [Press 2 then ENTER]\t\t\t\t\t\t\t\t\t|\n");
     print_walls(10);
     divider();
+
+    printf("Please enter a selection: ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        view_account();
+        break;
+    case 2:
+        new_user();
+        break;
+    default:
+        printf("Invalid choice.\n");
+        break;
+    }
 
 }
 
